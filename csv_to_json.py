edade5016c015848767a18af6d34c0348df78eb6
parker57 <parker57@uni.coventry.ac.uk>
2018-06-17 01:14:47 +0100

    changed to codec and made encoding utf-8

--- csv_to_json.py
+++ csv_to_json.py
@@ -5,9 +5,26 @@ import json
 
 #list(set(words_from('eng'))) unique words in English.
 
-with open("scrubbed.tsv") as scrubbed:
+with codecs.open("scrubbed.tsv", 'r', encoding='utf-8',) as scrubbed:
     tsv = csv.reader(scrubbed, delimiter='\t')
-    words = list(tsv)
+    word_rows = list(tsv)
+
+# Removing orthographic relationships and converting lists to strings
+# words = [w[0]+'@#&'+w[2] for w in words if w[1][4:] != 'variant:orthography']
+words = []
+for wr in word_rows:
+    try:
+        if wr[1][4:] != 'variant:orthography':
+            words.append(wr[0]+'@#&'+wr[2])
+    except:
+        print('!',wr)
+        
+# Conversion to strings allows for removing duplicate entries
+# Many have the same associations stored in etymology and is_derived_from relations
+words = list(set(words))
+
+# Back into original form
+words = [w.split('@#&') for w in words]
 
 def languages():
     # Returns list of languages in data
@@ -17,30 +34,23 @@ roots = {}
 for l in languages():
     roots[l] = {}
 
-# Removing orthographic relationships and converting lists to strings
-words = [w[0]+'@#&'+w[2] for w in words if w[1][4:] != 'variant:orthography']
-
-# Conversion to strings allows for removing duplicate entries
-# Many have the same associations stored in etymology and is_derived_from relations
-words = list(set(words))
-
-# Back into original form
-words = [w.split('@#&') for w in words]
-
 def add_row(row):
-    w = row[0].split(' ',1)[1]
-    w_lang = re.search(r'[^:]*', row[0]).group(0)
-    rw = row[1].split(' ',1)[1]
-    rw_lang = re.search(r'[^:]*', row[1]).group(0)
-    if w in roots[w_lang]:
-        roots[w_lang][w].append({rw:rw_lang})
-    else:
-        roots[w_lang][w] = [{rw:rw_lang}]
+    try:
+        w = row[0].split(' ',1)[1]
+        w_lang = re.search(r'[^:]*', row[0]).group(0)
+        rw = row[1].split(' ',1)[1]
+        rw_lang = re.search(r'[^:]*', row[1]).group(0)
+        if w in roots[w_lang]:
+            roots[w_lang][w].append({rw:rw_lang})
+        else:
+            roots[w_lang][w] = [{rw:rw_lang}]
+    except:
+        print('!',row)
 
 for w in words:
     add_row(w)
 
-with open('etymologies.json','w') as f:
+with codecs.open('etymologies.json', 'w', encoding='utf-8') as f:
     json.dump(roots, f)
     
 
